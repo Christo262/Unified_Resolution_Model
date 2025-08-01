863e737174b49477aa62d496dcd2a9ac38194ca8
Christo Cilliers <cilliers262@live.com>
2025-08-01 07:35:57 +0200

    Built new UI in blazor WASM, added three.js concept illustration

---
+++ src/dim_e/Urm.DimE/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Urm.DimE.Pages
+{
+    public partial class Home
+    {
+        [Inject] private IJSRuntime _js { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+            await _js.InvokeVoidAsync("initOverviewAnim", "urm-visual-container");
+        }
+    }
+}
